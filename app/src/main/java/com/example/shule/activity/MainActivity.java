bc74181c26b4784560ef3a5819bb15e07feebdd6
unknown <gatheruteving@gmail.com>
2020-09-27 12:30:31 +0300

    create connection class

--- app/src/main/java/com/example/shule/activity/MainActivity.java
+++ app/src/main/java/com/example/shule/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.shule;
+package com.example.shule.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.shule.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/shule/connection/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package com.example.shule.connection;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+/**
+ * This class facilitates a connection to the database
+ */
+public class DatabaseConnection extends SQLiteOpenHelper {
+    private static final String name = "shule";
+    private static final int version = 1;
+
+    public DatabaseConnection(@Nullable Context context) {
+        super(context, name, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL();
+        sqLiteDatabase.execSQL();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
