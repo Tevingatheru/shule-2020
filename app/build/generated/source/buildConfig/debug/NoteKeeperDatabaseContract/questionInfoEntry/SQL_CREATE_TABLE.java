e728c6b43506d191100866b0f1081f55bd569c2e
Lucy010-coder <66473330+Lucy010-coder@users.noreply.github.com>
2020-10-27 22:10:05 +0300

    build

--- app/build/generated/source/buildConfig/debug/NoteKeeperDatabaseContract/questionInfoEntry/SQL_CREATE_TABLE.java
+++
@@ -1,4 +0,0 @@
-package NoteKeeperDatabaseContract.questionInfoEntry;
-
-public class SQL_CREATE_TABLE {
-}
--- app/build/generated/source/buildConfig/debug/NoteKeeperDatabaseContract/topicInfoEntry/SQL_CREATE_TABLE.java
+++
@@ -1,4 +0,0 @@
-package NoteKeeperDatabaseContract.topicInfoEntry;
-
-public class SQL_CREATE_TABLE {
-}
