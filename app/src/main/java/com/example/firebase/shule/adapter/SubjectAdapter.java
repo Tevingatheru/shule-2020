de2bd44855fb906e3470e0daea45762dd2583b7f
unknown <gatheruteving@gmail.com>
2020-10-29 14:22:05 +0300

    onClick

--- app/src/main/java/com/example/firebase/shule/adapter/SubjectAdapter.java
+++ app/src/main/java/com/example/firebase/shule/adapter/SubjectAdapter.java
@@ -1,5 +1,6 @@
 package com.example.firebase.shule.adapter;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.firebase.shule.R;
+import com.example.firebase.shule.activity.TopicActivity;
 import com.example.firebase.shule.model.Subject;
 import com.example.firebase.shule.util.FirebaseUtil;
 import com.google.firebase.database.ChildEventListener;
@@ -23,7 +25,6 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectViewHolder> {
@@ -31,6 +32,7 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectV
     public static FirebaseDatabase firebaseDatabase;
     public static DatabaseReference databaseReference;
     private ChildEventListener childEventListener;
+    private static ArrayList<Subject> subjectList;
 
 
     public SubjectAdapter() {
@@ -93,8 +95,8 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectV
     @Override
     public void onBindViewHolder(@NonNull SubjectViewHolder holder, int position) {
         Log.d("Position: ", "Position Of Subject" + position);
-        List<Subject> subjectlist = new ArrayList<>(subjectSet);
-        Subject subject = subjectlist.get(position);
+        subjectList = new ArrayList<>(subjectSet);
+        Subject subject = subjectList.get(position);
         holder.bind(subject);
     }
 
@@ -119,11 +121,18 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectV
             super(itemView);
             tvSubject = (TextView) itemView.findViewById(R.id.row_subject_tv);
             ivImageSubject = (ImageView) itemView.findViewById(R.id.row_subject_iv);
+
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-
+            int position = getAdapterPosition();
+            Log.d("Position: ", "Positin Clicked is: " + position);
+            Subject subject = subjectList.get(position);
+            Intent intent = new Intent(v.getContext(), TopicActivity.class);
+            intent.putExtra("Subject", subject);
+            v.getContext().startActivity(intent);
         }
 
         public void bind (Subject subject) {
@@ -139,8 +148,8 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectV
                 Log.d("Image: ", imageUri);
                 Picasso.get()
                         .load(fileUri)
-//                        .resize(100,100)
-//                        .centerCrop()
+                        .resize(120,120)
+                        .centerCrop()
                         .into(ivImageSubject);
             }
         }
